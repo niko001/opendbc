e2d8205043c7df89794b60082fed3ed4ace3e2ae
niko001 <nikolaus_fischer@t-online.de>
2025-08-04 19:38:26 +0200

    fix comment

--- opendbc/safety/safety.h
+++ opendbc/safety/safety.h
@@ -334,7 +334,7 @@ void safety_tick(const safety_config *cfg) {
       cfg->rx_checks[i].status.lagging = lagging;
       if (lagging) {
         controls_allowed = false;
-        #mads_exit_controls(MADS_DISENGAGE_REASON_LAG);
+        // mads_exit_controls(MADS_DISENGAGE_REASON_LAG);
       }
 
       if (lagging || !is_msg_valid(cfg->rx_checks, i)) {
@@ -381,7 +381,7 @@ static void stock_ecu_check(bool stock_ecu_detected) {
   if ((safety_mode_cnt > RELAY_TRNS_TIMEOUT) && stock_ecu_detected) {
     relay_malfunction_set();
   }
-  #mads_state_update(vehicle_moving, acc_main_on, controls_allowed, brake_pressed || regen_braking, steering_disengage);
+  // mads_state_update(vehicle_moving, acc_main_on, controls_allowed, brake_pressed || regen_braking, steering_disengage);
 }
 
 static void relay_malfunction_reset(void) {
